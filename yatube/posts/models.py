2223c566f7eba7486550e9a900904cbd122c839b
Кирилл Черненко <Chernenko.kiry@yandex.ru>
2022-10-19 19:03:54 +0300

    final

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,8 +1,6 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth import get_user_model
+
 
 User = get_user_model()
 
@@ -16,13 +14,16 @@ class Post(models.Model):
         related_name='posts'
     )
     group = models.ForeignKey('Group', blank=True, null=True,
-                              on_delete=models.CASCADE)
+                              on_delete=models.SET_NULL, related_name='posts')
+    class Meta:
+        ordering = ['-pub_date']
 
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+
 
     def __str__(self) -> str:
         return self.title
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+POSTS_IN_PAGE: int = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:POSTS_IN_PAGE]
     context = {
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
 
-
+    
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = posts = Post.objects.filter(group=group).order_by('pub_date')[:POSTS_IN_PAGE]
     context = {
         'group': group,
         'posts': posts,
