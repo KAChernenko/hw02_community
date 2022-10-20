4ec44e518b2453ca6c9a773ca5db77751179ceea
Кирилл Черненко <Chernenko.kiry@yandex.ru>
2022-10-20 14:36:36 +0300

    final

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('pub_date')[:POSTS_IN_P]
+    posts = group.posts.all()[:POSTS_IN_P]
     context = {
         'group': group,
         'posts': posts,
